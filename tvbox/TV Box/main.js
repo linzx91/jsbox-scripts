2c5c01ea3f258f11584d44f30434e493e34e71c4
Aligege <linzx91@users.noreply.github.com>
2020-09-27 14:09:41 +0800

    Update TV Box.box

--- tvbox/TV Box/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const app = require("/scripts/app");
-
-$app.env !== $env.app ? $app.openURL("jsbox://run?name=" + encodeURI($addin.current.name)) : $ui.render(app.window);
-
-$app.rotateDisabled = $device.isIpad || $device.isIpadPro ? false : true;
